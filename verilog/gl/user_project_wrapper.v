86121829953841fbbfc8c9fd62a053254df996e3
lrburle <landonburleson203@gmail.com>
2023-11-07 13:02:01 -0600

    The input and output pins are routing in the GDS layout of the carvel. Removing the extra lib file for the ring oscillators seems to do wonders for the flow.

--- verilog/gl/user_project_wrapper.v
+++ verilog/gl/user_project_wrapper.v
@@ -1,18 +1,12 @@
 module user_project_wrapper (user_clock2,
-    vccd1,
-    vccd2,
-    vdda1,
-    vdda2,
-    vssa1,
-    vssa2,
-    vssd1,
-    vssd2,
     wb_clk_i,
     wb_rst_i,
     wbs_ack_o,
     wbs_cyc_i,
     wbs_stb_i,
     wbs_we_i,
+    vssd1,
+    vccd1,
     analog_io,
     io_in,
     io_oeb,
@@ -26,20 +20,14 @@ module user_project_wrapper (user_clock2,
     wbs_dat_o,
     wbs_sel_i);
  input user_clock2;
- input vccd1;
- input vccd2;
- input vdda1;
- input vdda2;
- input vssa1;
- input vssa2;
- input vssd1;
- input vssd2;
  input wb_clk_i;
  input wb_rst_i;
  output wbs_ack_o;
  input wbs_cyc_i;
  input wbs_stb_i;
  input wbs_we_i;
+ input vssd1;
+ input vccd1;
  inout [28:0] analog_io;
  input [37:0] io_in;
  output [37:0] io_oeb;
@@ -93,4 +81,15 @@ module user_project_wrapper (user_clock2,
     io_in[8],
     io_in[7],
     io_in[6]}));
+ sky130_osu_ring_oscillator_mpr2ca_8_b0r1 ro1 (.s1(io_in[0]),
+    .s2(io_in[1]),
+    .s3(io_in[2]),
+    .s4(io_in[3]),
+    .s5(io_in[4]),
+    .X5_Y1(\a[4] ),
+    .X4_Y1(\a[3] ),
+    .X3_Y1(\a[2] ),
+    .X2_Y1(\a[1] ),
+    .X1_Y1(\a[0] ),
+    .start(io_in[5]));
 endmodule
